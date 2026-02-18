04f0b76effa2304907ea15e03dbb5b6357e33048
Md Khayrul Hasan <khayrul.web@gmail.com>
2026-02-18 18:51:07 +0600

    Update last commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuration file for sensitive data
+// This file should NOT be committed to version control
+// Get your credentials from https://www.emailjs.com/
+
+const CONFIG = {
+    emailjs: {
+        serviceId: 'service_locvdra',
+        templateId: 'template_vgo6eb9',
+        publicKey: 'Miyj13RhV53SlpsgN'
+    }
+};
