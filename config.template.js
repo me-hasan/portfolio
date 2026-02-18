2a2eb18982d2d9f1ed801ce8358ba26fea039b9d
Md Khayrul Hasan <khayrul.web@gmail.com>
2026-02-18 19:04:43 +0600

    added hithub actions

---
+++ config.template.js
@@ -0,0 +1,8 @@
+// Configuration file - secrets injected during build
+const CONFIG = {
+    emailjs: {
+        serviceId: '%EMAILJS_SERVICE_ID%',
+        templateId: '%EMAILJS_TEMPLATE_ID%',
+        publicKey: '%EMAILJS_PUBLIC_KEY%'
+    }
+};
